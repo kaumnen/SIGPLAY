c1d1e8e9fc833fdbdf81af5326b658ce36927207
kaumnen <dev@kaumnen.com>
2025-11-11 14:39:59 +0000

    update init file

--- views/__init__.py
+++ views/__init__.py
@@ -1,4 +1,5 @@
 from .library import LibraryView
 from .now_playing import NowPlayingView
+from .visualizer import VisualizerView
 
-__all__ = ["LibraryView", "NowPlayingView"]
+__all__ = ["LibraryView", "NowPlayingView", "VisualizerView"]
