ebe5cc30dd14f3451af4b16c1771127e44cb06b1
kaumnen <dev@kaumnen.com>
2025-11-17 23:53:38 +0100

    remove old visualizer

--- views/__init__.py
+++ views/__init__.py
@@ -1,6 +1,5 @@
 from .library import LibraryView
 from .now_playing import NowPlayingView
-from .visualizer import VisualizerView
 from .meters import MetersView
 
-__all__ = ["LibraryView", "NowPlayingView", "VisualizerView", "MetersView"]
+__all__ = ["LibraryView", "NowPlayingView", "MetersView"]
